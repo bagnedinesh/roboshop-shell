462168cd4a1d637db48004be2465f2c8131efd3f
bagnedinesh <dineshbagne@gmail.com>
2023-01-06 10:20:43 +0530

    shell practice

--- frontend.sh
+++ frontend.sh
@@ -23,7 +23,7 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
 #Create Nginx Reverse Proxy Configuration
-cp ${Script_Location}/files/ngin-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${Script_Location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 #Restart Nginx Service to load the changes of the configuration.
 systemctl restart nginx
