f9137893040ef56b85a914ba64d32da71664b9c4
bagnedinesh <dineshbagne@gmail.com>
2023-01-06 10:24:19 +0530

    shell practice

--- frontend.sh
+++ frontend.sh
@@ -13,7 +13,7 @@ systemctl enable nginx
 systemctl start nginx
 
 #Remove the default content that web server is serving.
-rm -rf /usr/share/nginx/html*
+rm -rf /usr/share/nginx/html/*
 
 #Download the frontend content
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
