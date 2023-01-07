3d372202861223a415fdc1929d6877a62f0f3447
bagnedinesh <dineshbagne@gmail.com>
2023-01-07 19:55:51 +0530

    updating ./roboshop-shell/roboshop-shell

--- catalogue.sh
+++ catalogue.sh
@@ -1,44 +1,45 @@
 #Catalogue is a microservice that is responsible for serving the list of items
 # that displays in roboshop application.
+log=/tmp/roboshop.log
 script_location=$(pwd)
 #Setup NodeJS repos
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
 
 #Install NodeJS
-yum install nodejs -y
+yum install nodejs -y &>>${log}
 
 #Add application User
-useradd roboshop
+useradd roboshop &>>${log}
 
 #setup an app directory
-mkdir -p /app
+mkdir -p /app &>>${log}
 
 #Download the application code
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-rm -rf /app/*
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log}
+rm -rf /app/* &>>${log}
 cd /app
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip &>>${log}
 
 # download the dependencies
 cd /app
-npm install
+npm install &>>${log}
 
 #Setup SystemD Catalogue Service
-cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service
+cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service &>>${log}
 
 #load and start the service
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
+systemctl daemon-reload &>>${log}
+systemctl enable catalogue &>>${log}
+systemctl start catalogue &>>${log}
 
 #setup MongoDB repo
-cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log}
 
 #install mongodb-client
-yum install mongodb-org-shell -y
+yum install mongodb-org-shell -y &>>${log}
 
 #Load Schema
-mongo --host mongodb-dev.dineshbagne.tech </app/schema/catalogue.js
+mongo --host mongodb-dev.dineshbagne.tech </app/schema/catalogue.js &>>${log}
 
 
 
