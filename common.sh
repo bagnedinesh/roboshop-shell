6799c2b298abb22df45a30f109e679020073620e
bagnedinesh <dineshbagne@gmail.com>
2023-01-10 07:21:59 +0530

    updating ./roboshop-shell/roboshop-shell

--- common.sh
+++ common.sh
@@ -2,8 +2,8 @@ Script_Location=$(pwd)
 log=/tmp/roboshop.log
 status_check(){
   if [ $? -eq 0 ]
-  then echo -e "\e[32mSUCCESS\e[0m"
-  else echo -e "\e[31mFAILURE\e[0m"
+  then echo -e "\e[1;32mSUCCESS\e[0m"
+  else echo -e "\e[1;31mFAILURE\e[0m"
   exit
   echo "Refer log file for more information Log - $log"
   fi
