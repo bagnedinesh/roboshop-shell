bce0d1a0696a24f85ece91b6a1d3cebcb6e65892
bagnedinesh <dineshbagne@gmail.com>
2023-01-10 10:38:39 +0530

    updating ./roboshop-shell/roboshop-shell

--- common.sh
+++ common.sh
@@ -4,8 +4,9 @@ status_check(){
   if [ $? -eq 0 ]
   then echo -e "\e[1;32mSUCCESS\e[0m"
   else echo -e "\e[1;31mFAILURE\e[0m"
-  exit
+  
   echo "Refer log file for more information Log - $log"
+  exit
   fi
 }
 
