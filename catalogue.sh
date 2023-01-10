c3d82b18d8f119d11a186eb16d8423e8c58c020e
bagnedinesh <dineshbagne@gmail.com>
2023-01-10 10:32:00 +0530

    updating ./roboshop-shell/roboshop-shell

--- catalogue.sh
+++ catalogue.sh
@@ -1,45 +1,45 @@
 #Catalogue is a microservice that is responsible for serving the list of items
 # that displays in roboshop application.
 source common.sh
-echo -e "\e[35m Setup NodeJS repos \e[0m"
+print_head "Setup NodeJS repos"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
 status_check
-echo -e "\e[35m Install NodeJS \e[0m"
+print_head "Install NodeJS"
 yum install nodejs -y &>>${log}
 status_check
-echo -e "\e[35m Add application User \e[0m"
+print_head "Add application User"
 useradd roboshop &>>${log}
 status_check
-echo -e "\e[35m setup an app directory \e[0m"
+print_head "setup an app directory"
 mkdir -p /app &>>${log}
 status_check
-echo -e "\e[35m Download the application code \e[0m"
+print_head "Download the application code"
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log}
 rm -rf /app/* &>>${log}
 cd /app
 unzip /tmp/catalogue.zip &>>${log}
 status_check
-echo -e "\e[35m download the dependencies \e[0m"
+print_head "download the dependencies"
 cd /app
 npm install &>>${log}
 status_check
 
-echo -e "\e[35m Setup SystemD Catalogue Service \e[0m"
+print_head "Setup SystemD Catalogue Service"
 cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service &>>${log}
 status_check
-echo -e "\e[35m load and start the service \e[0m"
+print_head "load and start the service"
 systemctl daemon-reload &>>${log}
 systemctl enable catalogue &>>${log}
 systemctl start catalogue &>>${log}
 status_check
 
-echo -e "\e[35m setup MongoDB repo \e[0m"
+print_head "setup MongoDB repo"
 cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log}
 status_check
-echo -e "\e[35m install mongodb-client \e[0m"
+print_head "install mongodb-client"
 yum install mongodb-org-shell -y &>>${log}
 status_check
-echo -e "\e[35m Load Schema \e[0m"
+print_head "Load Schema"
 mongo --host mongodb-dev.dineshbagne.tech </app/schema/catalogue.js &>>${log}
 status_check
 
--- mongodb.sh
+++ mongodb.sh
@@ -1,18 +1,19 @@
 #MongoDB
 
-script_location=$(pwd)
-log=/tmp/roboshop.log
+source common.sh
 
-echo -e "\e[35m Set up mongodb repo \e[0m"
+print_head "Set up mongodb repo"
 cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log}
+status_check
 
-echo -e "\e[35m Install MongoDB \e[0m"
+print_head "Install MongoDB"
 yum install mongodb-org -y &>>${log}
-
-echo -e "\e[35m Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf \e[0m"
+status_check
+print_head "Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${log}
-
-echo -e "\e[35m Start & Enable MongoDB Service \e[0m"
+status_check
+print_head "Start & Enable MongoDB Service"
 systemctl enable mongod &>>${log}
 systemctl restart mongod &>>${log}
+status_check
 
