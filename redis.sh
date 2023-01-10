b4e0ffe105969a99bf98a5b65f2b3b97f727a057
bagnedinesh <dineshbagne@gmail.com>
2023-01-10 12:05:48 +0530

    updating ./roboshop-shell/roboshop-shell

--- redis.sh
+++ redis.sh
@@ -1,15 +1,15 @@
 source common.sh
 
 print_head "install rpm for Redis"
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${log}
 status_check
 
 print_head "Enable Redis 6.2 from package streams"
-dnf module enable redis:remi6.2 -y
+dnf module enable redis:remi6.2 -y &>>${log}
 status_check
 
 print_head "Install redis"
-yum install redis -y
+yum install redis -y &>>${log}
 status_check
 
 print_head "Update listen address from 127.0.0.1 to 0.0.0.0"
@@ -17,6 +17,6 @@ sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${log}
 status_check
 
 print_head "Start & Enable Redis Service"
-systemctl enable redis
-systemctl start redis
+systemctl enable redis &>>${log}
+systemctl start redis &>>${log}
 status_check
